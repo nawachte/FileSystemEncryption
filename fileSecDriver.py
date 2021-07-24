b6402ef625d316d76c4ada060bd5605b23249556
Nicholas Wachter <nawachte@calpoly.edu>
2021-07-24 15:54:07 -0700

    sub directories are now included, inputs can be given via command line

--- fileSecDriver.py
+++ fileSecDriver.py
@@ -2,41 +2,67 @@ from SecureFile import SecureFile
 import os
 import sys
 
-inFolder = input("In folder name: ")
-while inFolder not in os.listdir(os.getcwd()):
-    print("\""+inFolder + "\" does not exist in the current directory.")
-    inFolder = input("In folder name: ")
-outFolder = input("Out folder name: ")
-while outFolder not in os.listdir(os.getcwd()):
-    print(inFolder + " does not exist in the current directory.")
-    outFolder = input("Out folder name: ")
-key_word = input("Key word: ")
-confirm = "n"
-while confirm != "y":
-    confirm = input("Confirm that the key word is \""+key_word+"\" (y/n): ")
-    if confirm != "y":
-        key_word = input("Key word: ")
-confirm = "n"
-# e_or_d = ""
-while confirm != "y":
-    e_or_d = input("Do you want to encrypt(e) or decrypt(d) your files?: ")
-    while (e_or_d != "e") and (e_or_d != "d"):
-        print("Enter either \"e\" for encrypt or \"d\" for decrypt.")
-        e_or_d = input("Do you want to encrypt(e) or decrypt(d) your files?: ")
-    if e_or_d == "e":
-        confirm = input("Confirm that you would like to ENCRYPT your files (y/n): ")
-    else:
-        confirm = input("Confirm that you would like to DECRYPT your files (y/n): ")
+'''
+For entering arguments through the command line:
+python3 fileSecDriver.py <inFolder> <outFolder> <keyword> <e/d(encryption or decryption)>
+'''
 
-def encrypt_decrypt(cwd):
-    inFolder = cwd + "/" + inFolder
-    outFolder = cwd + "/" +outFolder
-    fileS = SecureFile(key_word)
+inFolder = None
+outFolder = None
+keyword = None
+e_or_d = None
+
+if len(sys.argv) < 2:
+    inFolder = input("In folder name: ")
+    while inFolder not in os.listdir(os.getcwd()):
+        print("\""+inFolder + "\" does not exist in the current directory.")
+        inFolder = input("In folder name: ")
+    outFolder = input("Out folder name: ")
+    while outFolder not in os.listdir(os.getcwd()):
+        print(outFolder + " does not exist in the current directory.")
+        outFolder = input("Out folder name: ")
+    key_word = input("Key word: ")
+    confirm = "n"
+    while confirm != "y":
+        confirm = input("Confirm that the key word is \""+key_word+"\" (y/n): ")
+        if confirm != "y":
+            key_word = input("Key word: ")
+    confirm = "n"
+    # e_or_d = ""
+    while confirm != "y":
+        e_or_d = input("Do you want to encrypt(e) or decrypt(d) your files?: ")
+        while (e_or_d != "e") and (e_or_d != "d"):
+            print("Enter either \"e\" for encrypt or \"d\" for decrypt.")
+            e_or_d = input("Do you want to encrypt(e) or decrypt(d) your files?: ")
+        if e_or_d == "e":
+            confirm = input("Confirm that you would like to ENCRYPT your files (y/n): ")
+        else:
+            confirm = input("Confirm that you would like to DECRYPT your files (y/n): ")
+
+else:
+    inFolder = sys.argv[1]
+    outFolder = sys.argv[2]
+    key_word = sys.argv[3]
+    e_or_d = sys.argv[4]
+
+fileS = SecureFile(key_word)
+
+def encrypt_decrypt(inFolder, outFolder):
     fileS.setInDir(inFolder)
     fileS.setOutDir(outFolder)
     for file in os.listdir(inFolder):
-        # if os.path.isdir(file):
-        #     encrypt_decrypt(cwd+"/"+file)
+        if os.path.isdir(inFolder+"/"+file):
+            # save in and out folders
+            inFolderSave = inFolder
+            outFolderSave = outFolder
+            # create outFolder sub directory if necessary
+            if file not in os.listdir(outFolder):
+                os.mkdir(outFolder+"/"+file)
+            encrypt_decrypt(inFolder+"/"+file, outFolder+"/"+file)
+            inFolder = inFolderSave
+            outFolder = outFolderSave
+            fileS.setInDir(inFolder)
+            fileS.setOutDir(outFolder)
         elif file != ".DS_Store":
             fileS.setFile(file)
             if e_or_d == "e":
@@ -50,7 +76,7 @@ def encrypt_decrypt(cwd):
                 #should never get here
                 raise Exception("Encrypt / Decrypt not correctly set.")
 
-encrypt_decrypt(os.getcwd())
+encrypt_decrypt(os.getcwd()+"/"+inFolder, os.getcwd()+"/"+outFolder)
 
 if e_or_d == "e":
     print("Encryption process complete.")
